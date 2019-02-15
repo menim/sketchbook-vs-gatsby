a544991588ba57ea5c75f4f97ceb6a88c2b1d827
menim <mel198629@gmail.com>
2019-02-15 21:11:15 +0200

    wrap content on 404 page with container component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,13 @@
 import React from "react"
 import SEO from "../components/seo"
+import Container from '../components/container'
 
 const NotFoundPage = () => (
-  <div className="l-container">
+    <Container>
     <SEO title="404: Not found" />
     <h1>Сторінка не існує</h1>
     <p>Ви перейшли на сторінку яка не існує</p>
-  </div>
+  </Container>
 )
 
 export default NotFoundPage
