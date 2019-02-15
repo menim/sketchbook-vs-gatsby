9de8cf6256ee635853080641a604af4ddc3add1d
menim <mel198629@gmail.com>
2019-02-15 21:15:12 +0200

    add simple contact page with simple markup

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import Header from '../components/header'
+
+const Contact = () => (
+  <Fragment>
+    <Header />
+    <div>Hello from contact page</div>
+  </Fragment>
+)
+
+export default Contact
