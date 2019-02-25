4554460e6dfe5f2e3daa73c3be64116a4b81c380
menim <mel198629@gmail.com>
2019-02-25 21:39:48 +0200

    add  link in nav component on delivery page

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 const Nav = () => (
   <nav className="menu">
     <ul className="menu__list">
-      <li className="menu__item"><Link to="/" className="menu__link menu__link--red-orange-theme">Оплата і доставка</Link></li>
+      <li className="menu__item"><Link to="/delivery" className="menu__link menu__link--red-orange-theme">Оплата і доставка</Link></li>
       <li className="menu__item"><Link to="/contact/" className="menu__link menu__link--scooter-theme">Контакти</Link></li>
       <li className="menu__item"><Link to="/" className="menu__link menu__link--green-haze-theme">Замовити</Link></li>
     </ul>
