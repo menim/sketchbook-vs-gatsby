b11a9d9e54bca86cf8496a8aa73867f8cb90cc5a
menim <mel198629@gmail.com>
2019-02-25 10:58:39 +0200

    add wht-sketch component to project

---
+++ src/components/wht-sketch.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MultilineBg from '../components/multilineBg';
+
+const whtSketch = () => (
+  <div className="wht-sketch">
+    <h3 className="wht-sketch__header"><MultilineBg>Что такое обучающий скетчбук?</MultilineBg></h3>
+    <ul className="wht-sketch__list"> 
+      <li className="wht-sketch__item"><MultilineBg>Это учебное пособие по основам рисования, каллиграфии, дудлингу</MultilineBg></li>
+      <li className="wht-sketch__item"><MultilineBg>Это креативный блокнот для набросков, зарисовок, записей и рисования - можно творить, как только душе пожелается</MultilineBg></li>
+      <li className="wht-sketch__item"><MultilineBg>Это кладовая идей творческого человека, которую он создает сам, развивая свои таланты и совершенствуя навыки</MultilineBg></li>
+    </ul>
+  </div>
+)
+
+export default whtSketch
