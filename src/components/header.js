5b54457d9515e1f75718acb1f39ab02b4ae92d02
menim <mel198629@gmail.com>
2019-02-25 21:22:53 +0200

    add parallax effect to description in header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Parallax from "react-rellax"
 
 import mp4Video from "../video/sketch-ai.mp4"
 import webpVideo from "../video/sketch-ai.webm"
 
 import Nav from "./nav"
 import Logo from "./logo";
-import Container from './container'
-import MultilineBg from './multilineBg';
+import Container from "./container"
+import MultilineBg from "./multilineBg"
 
 const Header = ({videoEnabled }) => (
   videoEnabled ? 
@@ -24,20 +25,23 @@ const Header = ({videoEnabled }) => (
         <Logo />
         <Nav />
       </div>
-      <div className="header__description">
-        <h1 className="header__title">
-          <MultilineBg>
-            Скетчбук - книга для записів та замальовок.
-          </MultilineBg>
-        </h1>
-        <p className="header__content">
-          <MultilineBg>
-            Це книга для сучасних людей, що шукають яскравих, креативних форм розвитку особистості, прагнуть збагатити своє життя
-            художніми образами. За її допомогою кожний зможе відкрити для себе нові обрії творчої свідомості, розкрити потенціал
-            підсвідомості та вивільнити уяву. Довіртесь своїй інтуїцїї — і перетворіть пошуки себе в витвір мистецтва.
-          </MultilineBg>
-        </p>
-      </div>
+        <div className="header__description">
+        <Parallax speed={4}>
+          <h1 className="header__title">
+            <MultilineBg>
+              Скетчбук - книга для записів та замальовок.
+            </MultilineBg>
+          </h1>
+          <p className="header__content">
+            <MultilineBg>
+              Це книга для сучасних людей, що шукають яскравих, креативних форм розвитку особистості, прагнуть збагатити своє життя
+              художніми образами. За її допомогою кожний зможе відкрити для себе нові обрії творчої свідомості, розкрити потенціал
+              підсвідомості та вивільнити уяву. Довіртесь своїй інтуїцїї — і перетворіть пошуки себе в витвір мистецтва.
+            </MultilineBg>
+          </p>
+        </Parallax>
+        </div>
+
     </Container>
   </header>
   ) :
