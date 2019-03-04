f7792d071eb3d22acdeac0aa4467b3a29c32b3bc
menim <mel198629@gmail.com>
2019-03-04 14:46:47 +0200

    add  slider, price and  title to template from data

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,23 +1,34 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link} from "gatsby"
+
+
 import Header from "../components/header"
 import Footer from "../components/footer"
 import Main from "../components/main"
 import Container from "../components/container"
-import FlexContainer from "../components/flex-container"
+import SimpleSlider from "../components/slick-gallery";
 
 const ProductPage = ({data}) => {
- const product = data.allDataJson.edges[0].node
- document.body.classList.add('h-100')
- 
- return (
+const product = data.allDataJson.edges[0].node
+
+return (
     <>
       <Header />
         <Main>
           <Container>
-            <FlexContainer>
-              {product.title}
-            </FlexContainer>
+            <div className="product">
+              <section className="product__view">
+              <SimpleSlider imgs={product.spreadImg} />
+              </section>
+              <section className="product__description">
+                <h1 className="product__header">Скетчбук. {product.title}</h1>
+                <span className="product__price">₴ {product.price}</span>
+                <p className="product__overview">
+                   {product.description}
+                </p>
+                <Link to="" type="button" className="btn btn--lg">Замовити</Link>
+              </section>
+            </div>
           </Container>
         </Main>
       <Footer />
