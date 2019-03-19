f4b94780a3fe31b1342ad62a7780542f6eeb3644
menim <mel198629@gmail.com>
2019-03-19 12:59:25 +0200

    replace props class with height in for setting height of pict component

--- src/components/pict.js
+++ src/components/pict.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Pict = (props) => (
   <picture>
     <source srcSet={`${props.src}.webp`} type="image/webp" />
-    <img className={props.className} src={`${props.src}.${props.fallbackFormat}`} /> 
+    <img height={props.height} src={`${props.src}.${props.fallbackFormat}`} /> 
   </picture>
 )
 
