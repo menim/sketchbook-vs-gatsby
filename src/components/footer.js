26ab3aaeba470e5761a6a549ef10fd4f33ce82e4
menim <mel198629@gmail.com>
2019-04-01 18:26:30 +0300

    add styles and markup in footer component relative to maket

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,7 +14,7 @@ const Footer = () => (
         </section>
         <section className='footer__section'>
           <h4 className='footer__title'>
-            <FormattedMessage id='address-title' />
+            <FormattedMessage id='address-title' />:
           </h4>
           <address>
             <FormattedMessage id='city-title' /> <br/>
@@ -22,13 +22,13 @@ const Footer = () => (
           </address>
         </section>
         <section className='footer__section'>
-          <h4 className='footer__title'><FormattedMessage id='contact-page-title' /></h4>
+          <h4 className='footer__title'><FormattedMessage id='footer-manager-title' />:</h4>
           <a className='telephone' href='tel: +38066-754-95-64'>066-754-95-64</a>
           <a className='telephone' href='tel: +38097-025-8-646'>097-025-86-46</a>
           <a className='telephone' href='tel: +3093-139-167-04'>093-139-67-04</a>
         </section>
         <section className='footer__section'>
-          <h4 className='footer__title'>Email</h4>
+          <h4 className='footer__title'>Email:</h4>
           mail@okobook.com
         </section>
       </div>
