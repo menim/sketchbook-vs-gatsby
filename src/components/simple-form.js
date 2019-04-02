5dce8432fc7519eab7124f63db464c7ad268569f
menim <mel198629@gmail.com>
2019-04-02 10:51:49 +0300

    add styles relative to maket in simple-form component for contact page

--- src/components/simple-form.js
+++ src/components/simple-form.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
-import MultilineBg from './multilineBg'
 import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 
@@ -14,11 +14,11 @@ const contactSchema = Yup.object().shape({
   .required(<FormattedMessage id='error-void-message' />),
   message: Yup.string()
   .min(5, <FormattedMessage id='error-short-message' />)
-    .max(100, <FormattedMessage id='error-long-message' />)
+    .max(500, <FormattedMessage id='error-long-message' />)
   .required(<FormattedMessage id='error-void-message' />)
 })
 
-const SimpleForm = () => (
+const SimpleForm = ({ isContact }) => (
   <Formik 
    initialValues={{
      name: '',
@@ -29,24 +29,24 @@ const SimpleForm = () => (
    validationSchema = {contactSchema}
   >
     {({ errors, touched }) => (
-      <Form action='' className='form'>
+      <Form action='' className={isContact ? 'form contact__form' : 'form'}>
         <label className='form__label' htmlFor='name'>
-          <MultilineBg><FormattedMessage id='form-name-field' /></MultilineBg>
+          <FormattedMessage id='form-name-field' />*:
           <Field className={errors.name && touched.name ? 'form__input form__input--error' : 'form__input'} name='name' />
           {errors.name && touched.name ? (
             <div className='form__error'>{errors.name}</div>
           ) : null}
         </label>
         <label className='form__label' htmlFor='email'>
-          <MultilineBg><FormattedMessage id='form-email-field' /></MultilineBg>
+          <FormattedMessage id='form-email-field' />*:
           <Field className={errors.email && touched.email ? 'form__input form__input--error'  : 'form__input'} name='email' type='email' />
           {errors.email && touched.email ? (
             <div className='form__error'>{errors.email}</div>
           ) : null}
         </label>
         <label className='form__label' htmlFor='message'>
-          <MultilineBg><FormattedMessage id='form-message-field' /></MultilineBg>
-          <Field className={errors.message && touched.message ? 'form__input form__input--textarea form__input--error' : 'form__input form__input--textarea'} component='textarea' name='message' />
+          <FormattedMessage id='form-message-field' />*:
+          <Field className={errors.message && touched.message ? 'form__input form__input--textarea form__input--error' : 'form__input form__input--textarea'} name='message' />
           {errors.message && touched.message ? <div className="form__error">{errors.message}</div> : null}
         </label>
          <button className='form__btn btn btn--primary-theme' type='submit'><FormattedMessage id='button-sent-title' /></button>
@@ -56,3 +56,11 @@ const SimpleForm = () => (
  )
 
  export default SimpleForm
+
+ SimpleForm.defaultProp = {
+   isContact: false
+ }
+
+ SimpleForm.propTypes = {
+   isContact: PropTypes.bool
+ }
