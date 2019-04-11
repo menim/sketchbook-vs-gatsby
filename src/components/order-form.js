ff5225cda0d229b209fd753be9a868ba2a544b32
menim <mel198629@gmail.com>
2019-04-11 11:07:29 +0300

    fix bug relative to increase decrease quantity field

--- src/components/order-form.js
+++ src/components/order-form.js
@@ -56,19 +56,19 @@ class OrderForm extends Component {
 
   handleSubmit = (values, actions) => {
     actions.setSubmitting(false)
-   fetch(`/${this.props.locale}/`, {
-     method: 'POST',
-     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-     body: encode({ 'form-name': 'contact', ...values })
-   }).then((response) => {
-     if(response.status === 200) {
-       actions.setStatus({ success: <FormattedMessage id='success-message' /> })
-       setTimeout(() => {
-        actions.resetForm();
-        this.setState({count: 1})   
-      }, 2000)
-     } 
-   })
+    fetch(`/${this.props.locale}/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...values })
+    }).then((response) => {
+      if(response.status === 200) {
+        actions.setStatus({ success: <FormattedMessage id='success-message' /> })
+        setTimeout(() => {
+          actions.resetForm();
+          this.setState({count: 1})   
+        }, 2000)
+      } 
+    })
   }
 
   render() {
@@ -106,56 +106,58 @@ class OrderForm extends Component {
               message: '',
               email: ''
             }}
-            enableReinitialize={true}
             validateOnChange
             validationSchema = {this.props.order ? orderSchema : contactSchema}
             onSubmit={(values, actions) => this.handleSubmit(values, actions)}
             >
-            {({errors, touched, isSubmitting, status }) => (
-              <Form className='form form--order' name="contact" method='POST' netlify-honeypot='bot-field' data-netlify='true' noValidate>
-                <input type='hidden' name='form-name' value='contact' />
-                <fieldset>
-                  <legend className={order ? 'form__title' : 'visuallyhidden'}>Форма заказа</legend>
-                  <label className={order ? 'form__label form__label--select-wrapper' : 'visuallyhidden'} htmlFor='select-sketch'>
-                    <Field className='form__input form__input--select' component='select' name='selectSketch'>
-                      {listSketch}
-                    </Field>
-                  </label>
-                  <label className={order ? 'form__label' : 'visuallyhidden'} htmlFor='quantity'>
-                    <FormattedMessage id="form-quantity-title" />:
-                    <div>
-                      <Field className='form__input form__input--quantity' onChange={this.handleChange} value={this.state.count} name='quantity' type="number"  />
-                      <button className='form__control' onClick={this.increment} type='button'>+</button>
-                      <button className='form__control' onClick={this.decrement} type='button'>-</button>
-                    </div>
-                  </label>  
-                  <label className='form__label' htmlFor='name'>
-                    <FormattedMessage id='form-name-field' />*:
-                    <Field className={errors.name && touched.name ? 'form__input form__input--error' : 'form__input'} name='name' />
-                    {errors.name && touched.name ? <div className="form__error">{errors.name}</div> : null}
-                  </label>
-                  <label className={order ? 'visuallyhidden'  : 'form__label'} htmlFor='email'>
-                    <FormattedMessage id='form-email-field' />*:
-                    <Field className={errors.email && touched.email ? 'form__input form__input--error'  : 'form__input'} name='email' type='email' />
-                    {errors.email && touched.email ? (
-                      <div className='form__error'>{errors.email}</div>
-                    ) : null}
-                  </label>
-                  <label className={order ? 'visuallyhidden' : 'form__label'}  htmlFor='message'>
-                    <FormattedMessage id='form-message-field' />*:
-                    <Field className={errors.message && touched.message ? 'form__input form__input--textarea form__input--error' : 'form__input form__input--textarea'} name='message' />
-                    {errors.message && touched.message ? <div className="form__error">{errors.message}</div> : null}
-                  </label>
-                  <label className={order ? 'form__label' : 'visuallyhidden'}  htmlFor='telephone'>
-                    Телефон:*
-                    <Field  className={errors.telephone && touched.telephone ? 'form__input form__input--error' : 'form__input'} name='telephone' type="numeber" placeholder='' /> 
-                    {errors.telephone && touched.telephone ? <div className="form__error">{errors.telephone}</div> : null}
-                  </label>        
-                  <button className='form__btn btn btn--primary-theme' type='submit'>{order ? <FormattedMessage id='button-order-title' /> : <FormattedMessage id='button-sent-title' />}</button> 
-                  {status && status.success ? <div className='form__success-message'>{status.success}</div>  : ''}   
-                </fieldset>
-              </Form>
-            )}
+              {({errors, touched, isSubmitting, status, values }) => { 
+                values.quantity = this.state.count
+                return (
+                  <Form className='form form--order' name="contact" method='POST' netlify-honeypot='bot-field' data-netlify='true' noValidate>
+                  <input type='hidden' name='form-name' value='contact' />
+                  <fieldset>
+                    <legend className={order ? 'form__title' : 'visuallyhidden'}>Форма заказа</legend>
+                    <label className={order ? 'form__label form__label--select-wrapper' : 'visuallyhidden'} htmlFor='select-sketch'>
+                      <Field className='form__input form__input--select' component='select' name='selectSketch'>
+                        {listSketch}
+                      </Field>
+                    </label>
+                    <label className={order ? 'form__label' : 'visuallyhidden'} htmlFor='quantity'>
+                      <FormattedMessage id="form-quantity-title" />:
+                      <div>
+                        <Field className='form__input form__input--quantity' onChange={this.handleChange} value={this.state.count} name='quantity' type="number"  />
+                        <button className='form__control' onClick={this.increment} type='button'>+</button>
+                        <button className='form__control' onClick={this.decrement} type='button'>-</button>
+                      </div>
+                    </label>  
+                    <label className='form__label' htmlFor='name'>
+                      <FormattedMessage id='form-name-field' />*:
+                      <Field className={errors.name && touched.name ? 'form__input form__input--error' : 'form__input'} name='name' />
+                      {errors.name && touched.name ? <div className="form__error">{errors.name}</div> : null}
+                    </label>
+                    <label className={order ? 'visuallyhidden'  : 'form__label'} htmlFor='email'>
+                      <FormattedMessage id='form-email-field' />*:
+                      <Field className={errors.email && touched.email ? 'form__input form__input--error'  : 'form__input'} name='email' type='email' />
+                      {errors.email && touched.email ? (
+                        <div className='form__error'>{errors.email}</div>
+                      ) : null}
+                    </label>
+                    <label className={order ? 'visuallyhidden' : 'form__label'}  htmlFor='message'>
+                      <FormattedMessage id='form-message-field' />*:
+                      <Field className={errors.message && touched.message ? 'form__input form__input--textarea form__input--error' : 'form__input form__input--textarea'} name='message' />
+                      {errors.message && touched.message ? <div className="form__error">{errors.message}</div> : null}
+                    </label>
+                    <label className={order ? 'form__label' : 'visuallyhidden'}  htmlFor='telephone'>
+                      Телефон:*
+                      <Field  className={errors.telephone && touched.telephone ? 'form__input form__input--error' : 'form__input'} name='telephone' type="numeber" placeholder='' /> 
+                      {errors.telephone && touched.telephone ? <div className="form__error">{errors.telephone}</div> : null}
+                    </label>        
+                    <button className='form__btn btn btn--primary-theme' type='submit'>{order ? <FormattedMessage id='button-order-title' /> : <FormattedMessage id='button-sent-title' />}</button> 
+                    {status && status.success ? <div className='form__success-message'>{status.success}</div>  : ''}   
+                  </fieldset>
+                </Form>
+                )}
+              }
             </Formik>
           )}
         }  
