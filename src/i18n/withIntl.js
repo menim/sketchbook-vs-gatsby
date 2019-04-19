74d9a428a988c5a310eaed5cbcd8d0f7d2897715
menim <mel198629@gmail.com>
2019-04-19 07:43:32 +0300

    disable default locale en

--- src/i18n/withIntl.js
+++ src/i18n/withIntl.js
@@ -34,7 +34,7 @@ export default ComposedComponent => {
 
     render() {
       const { language } = this.state
-      const locale = language.locale || "en"
+      const locale = language.locale
       const messages = require(`./locales/${locale}.js`) // eslint-disable-line
       const IntlComponent = injectIntl(ComposedComponent)
       return (
