1b18dc2d848127babb35344efbf4c02f5ddc61e9
menim <mel198629@gmail.com>
2019-04-25 09:02:58 +0300

    add gatsby-plugin-netlify for working with netlify redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
         })],
       },
     },
-
+    `gatsby-plugin-netlify`,
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
