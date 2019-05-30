57d8b99abf93f654ba33e2d2d9f553736ec179c0
menim <mel198629@gmail.com>
2019-05-30 12:22:39 +0300

    Fix bug that appear when build gatsby project relative to get dom node in Modal component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Form from './order-form';
 
-
-const portalRoot = typeof document !== `undefined` ? document.getElementById('modal') : null;
+const portalRoot =
+  typeof document !== `undefined` ? document.getElementById('modal') : null;
 
 class Modal extends Component {
   constructor() {
@@ -23,19 +23,19 @@ class Modal extends Component {
   };
 
   render() {
-    if(this.el) {    
-    return ReactDOM.createPortal(
-      <div className={this.props.isShow ? '' : 'none'}>
-        <div className="backdrop" onClick={this.props.close} />
-        <div className="modal">
-          <button className="modal__close close" onClick={this.props.close} />
-          <Form order locale={this.props.locale} />
-        </div>
-      </div>,
-     this.el
-    );
+    if (this.el) {
+      return ReactDOM.createPortal(
+        <div className={this.props.isShow ? '' : 'none'}>
+          <div className='backdrop' onClick={this.props.close} />
+          <div className='modal'>
+            <button className='modal__close close' onClick={this.props.close} />
+            <Form order locale={this.props.locale} />
+          </div>
+        </div>,
+        this.el
+      );
     } else {
-      return null
+      return null;
     }
   }
 }
@@ -45,5 +45,5 @@ export default Modal;
 Modal.propTypes = {
   isShow: PropTypes.bool.isRequired,
   close: PropTypes.func.isRequired,
-  locale: PropTypes.string.isRequired
+  locale: PropTypes.string.isRequired,
 };
