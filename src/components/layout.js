d5fa342ff9273a727e5cb44c5ae513ee8d661f33
menim <mel198629@gmail.com>
2019-05-30 12:16:35 +0300

    Initialize state as custom class field instead initialize with constructor and pass toggleModal function instead state

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl } from 'react-intl';
+import {injectIntl} from 'react-intl';
 
-import { ModalContext } from './modal-context';
+import {ModalContext} from './modal-context';
 import Modal from './modal';
 
 class Layout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShow: false, toggleModal: this.toggleModal };
-  }
+  state = {isShow: false};
 
   toggleModal = () => {
     document.body.classList.contains('modal-open')
       ? document.body.classList.remove('modal-open')
       : document.body.classList.add('modal-open');
-    this.setState(prevState => ({ isShow: !prevState.isShow }));
+    this.setState(prevState => ({isShow: !prevState.isShow}));
   };
 
   render() {
     return (
-      <ModalContext.Provider value={this.state}>
+      <ModalContext.Provider value={this.toggleModal}>
         {this.props.children}
-          <Modal isShow={this.state.isShow ? true : false}  close={this.toggleModal} locale={this.props.intl.locale} />
-        )}
+        <Modal
+          isShow={this.state.isShow ? true : false}
+          close={this.toggleModal}
+          locale={this.props.intl.locale}
+        />
       </ModalContext.Provider>
     );
   }
@@ -33,5 +33,5 @@ export default injectIntl(Layout);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  intl: PropTypes.object.isRequired
+  intl: PropTypes.object.isRequired,
 };
