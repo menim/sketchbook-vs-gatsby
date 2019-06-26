c8cdd3e1ead9703d81faefe5e8cb5e3831a9dacd
menim <mel198629@gmail.com>
2019-06-26 12:06:23 +0300

    Add zoomIn/zoomOut animation when click on cartToggleBtn component

--- src/components/Cart/cartToggleBtn.js
+++ src/components/Cart/cartToggleBtn.js
@@ -5,17 +5,20 @@ import cart from '../../images/cart-icon.svg';
 
 const CartToggleBtn = ({status, toggle}) => (
   <button onClick={toggle} className='cart__toggle-btn'>
-    {status ? (
-      <img
-        className='cart__basket-img'
-        src={cart}
-        height='36'
-        width='36'
-        alt=''
-      />
-    ) : (
-      <span className='cart__close close close--theme-secondary' />
-    )}
+    <img
+      className={`cart__basket-img  ${
+        status ? 'animationZoomOut' : 'animationZoomIn'
+      } `}
+      src={cart}
+      height='36'
+      width='36'
+      alt=''
+    />
+    <span
+      className={`cart__close close close--theme-secondary ${
+        status ? 'animationZoomIn' : 'animationZoomOut'
+      } `}
+    />
   </button>
 );
 
