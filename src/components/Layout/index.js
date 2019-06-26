7a5bf014b2b9b220189b16114215c9ea0ffcfeb4
menim <mel198629@gmail.com>
2019-06-26 16:36:34 +0300

    Decouple toggleModal function in Layout on two and save them in new extended state.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,29 +2,44 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {injectIntl, FormattedTime} from 'react-intl';
 
+import Cart from '../Cart';
 import Footer from './footer';
 
 import {ModalContext} from '../../context/modalContext';
 import Modal from './modal';
 
 class Layout extends Component {
-  state = {isShow: false};
-
-  toggleModal = () => {
-    document.body.classList.contains('modal-open')
-      ? document.body.classList.remove('modal-open')
-      : document.body.classList.add('modal-open');
-    this.setState(prevState => ({isShow: !prevState.isShow}));
+  state = {
+    interface: {
+      hideScroll: () => {
+        document.body.classList.contains('modal-open')
+          ? document.body.classList.remove('modal-open')
+          : document.body.classList.add('modal-open');
+      },
+    },
+    modal: {
+      isShow: false,
+      toggle: () => {
+        this.setState(prevState => ({
+          modal: {
+            ...prevState.modal,
+            isShow: !prevState.modal.isShow,
+          },
+        }));
+        this.state.interface.hideScroll();
+      },
+    },
   };
 
   render() {
     return (
-      <ModalContext.Provider value={this.toggleModal}>
+      <ModalContext.Provider value={this.state.modal.toggle}>
+        <Cart />
         {this.props.children}
         <Footer />
         <Modal
-          isShow={this.state.isShow}
-          close={this.toggleModal}
+          isShow={this.state.modal.isShow}
+          close={this.state.modal.toggle}
           locale={this.props.intl.locale}
         />
       </ModalContext.Provider>
