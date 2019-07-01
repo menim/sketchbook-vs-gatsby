9af2e5f9f030ded7f2e48f1bdc5affdce9fbd422
menim <mel198629@gmail.com>
2019-07-01 10:14:16 +0300

    Make seperate options-list sass component part of form component

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -21,100 +21,98 @@ import Button from '../components/shared/button';
 const ProductPage = ({data, intl}) => {
   const product = data.allDataJson.edges[0].node;
   return (
-    <>
-      <Layout>
-        {/* <Seo 
+    <Layout>
+      {/* <Seo 
         description={`${intl.messages.productPageDescriptionTemplatePre}, «${intl.messages.title}», ${intl.messages.productPageDescriptionTemplatePost}`}
         title={`${intl.messages.title}. ${intl.messages.productPageTitleTemplate}`}
         lang={intl.locale}
         img={product.cover}
         url={product.slug}
       /> */}
-        <Header />
-        <Main smVerticalOffset>
-          <Container>
-            <Button
-              as={Link}
-              to="/"
-              theme="secondary"
-              className="l-container__btn"
-            >
-              <FormattedMessage id="return-main-title" />
-            </Button>
-            <div className="product">
-              <section className="product__view">
-                <ProductItemSlider imgs={product.spreadImg} />
-              </section>
-              <section className="product__description">
-                <h1 className="product__header">
-                  Скетчбук. <FormattedMessage id="title" />
-                </h1>
-                <span className="product__price">₴ {product.price}</span>
-                <p className="product__overview">
-                  <FormattedMessage id="description" />
-                </p>
-                <form className="productForm" onSubmit={console.log('test')}>
-                  <fieldset className="options-list product__options-list">
-                    <legend className="options-list__title">
-                      <FormattedMessage id="lang-option-title" />:
-                    </legend>
-                    {product[intl.locale].lang.map((lang, index) => (
-                      <div className="options-list__item" key={index}>
-                        <input
-                          className="options-list__input sr-only"
-                          id={`lang${index}`}
-                          type="radio"
-                          name="lang"
-                          value={lang}
-                          defaultChecked={index === 0 ? true : false}
-                        />
-                        <label
-                          className="options-list__label"
-                          htmlFor={`lang${index}`}
-                        >
-                          {lang}
-                        </label>
-                      </div>
-                    ))}
-                  </fieldset>
-                  <fieldset className="options-list product__options-list">
-                    <legend className="options-list__title">
-                      <FormattedMessage id="cover-option-title" />:
-                    </legend>
-                    {product.cover.map((cover, index) => (
-                      <div className="options-list__item" key={index}>
-                        <input
-                          className="options-list__input sr-only"
-                          id={`${product.color[index]}`}
-                          type="radio"
-                          name="cover"
-                          value={`${cover.childImageSharp.fixed.src} ${
-                            product.color[index]
-                          }`}
-                          defaultChecked={index === 0 ? true : false}
-                        />
-                        <label
-                          className="options-list__label"
-                          htmlFor={`${product.color[index]}`}
-                        >
-                          <img src={cover.childImageSharp.fixed.src} alt="" />
-                        </label>
-                      </div>
-                    ))}
-                  </fieldset>
-                  <Button theme="primary">
-                    <FormattedMessage id="cart-btn-title" />
-                  </Button>
-                </form>
-                <p className="product__paragraph">
-                  <FormattedMessage id="wholesale-message" />
-                </p>
-              </section>
-            </div>
-          </Container>
-        </Main>
-      </Layout>
-    </>
+      <Header />
+      <Main smVerticalOffset>
+        <Container>
+          <Button
+            as={Link}
+            to="/"
+            theme="secondary"
+            className="l-container__btn"
+          >
+            <FormattedMessage id="return-main-title" />
+          </Button>
+          <div className="product">
+            <section className="product__view">
+              <ProductItemSlider imgs={product.spreadImg} />
+            </section>
+            <section className="product__description">
+              <h1 className="product__header">
+                Скетчбук. <FormattedMessage id="title" />
+              </h1>
+              <span className="product__price">₴ {product.price}</span>
+              <p className="product__overview">
+                <FormattedMessage id="description" />
+              </p>
+              <form className="form" onSubmit={console.log('test')}>
+                <fieldset className="form__radio-list product__options-list">
+                  <legend className="form__legend form__legend--with-triangle">
+                    <FormattedMessage id="lang-option-title" />:
+                  </legend>
+                  {product[intl.locale].lang.map((lang, index) => (
+                    <div className="form__item-wrapper" key={index}>
+                      <input
+                        className="form__input sr-only"
+                        id={`lang${index}`}
+                        type="radio"
+                        name="lang"
+                        value={lang}
+                        defaultChecked={index === 0 ? true : false}
+                      />
+                      <label
+                        className="form__label form__label--radio-btn"
+                        htmlFor={`lang${index}`}
+                      >
+                        {lang}
+                      </label>
+                    </div>
+                  ))}
+                </fieldset>
+                <fieldset className="form__radio-list product__options-list">
+                  <legend className="form__legend form__legend--with-triangle">
+                    <FormattedMessage id="cover-option-title" />:
+                  </legend>
+                  {product.cover.map((cover, index) => (
+                    <div className="form__item-wrapper" key={index}>
+                      <input
+                        className="form__input sr-only"
+                        id={`${product.color[index]}`}
+                        type="radio"
+                        name="cover"
+                        value={`${cover.childImageSharp.fixed.src} ${
+                          product.color[index]
+                        }`}
+                        defaultChecked={index === 0 ? true : false}
+                      />
+                      <label
+                        className="form__label form__label--radio-btn"
+                        htmlFor={`${product.color[index]}`}
+                      >
+                        <img src={cover.childImageSharp.fixed.src} alt="" />
+                      </label>
+                    </div>
+                  ))}
+                </fieldset>
+                <Button theme="primary">
+                  <FormattedMessage id="cart-btn-title" />
+                </Button>
+              </form>
+              <p className="product__paragraph">
+                <FormattedMessage id="wholesale-message" />
+              </p>
+            </section>
+          </div>
+        </Container>
+      </Main>
+    </Layout>
   );
 };
 
