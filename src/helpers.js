9771b9c1c1818334f5659c2b73cea0ee31101bdf
menim <mel198629@gmail.com>
2019-07-01 14:24:11 +0300

    Add uniqueId method in helpers file that return unique string

--- src/helpers.js
+++ src/helpers.js
@@ -5,3 +5,15 @@ export const encode = data => {
 };
 
 export const phoneRegExp = /^\+?3?8?(0\d{9})$/;
+
+export const uniqueId = function() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return (
+    '_' +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  );
+};
