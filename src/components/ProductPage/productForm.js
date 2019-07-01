91838643f6165f42df5072ccb0960b4be4b20262
menim <mel198629@gmail.com>
2019-07-01 13:22:40 +0300

    Includue StoreContext in ProductForm component

--- src/components/ProductPage/productForm.js
+++ src/components/ProductPage/productForm.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {FormattedMessage} from 'react-intl';
 
+import {StoreContext} from '../../context/storeContext';
+
 import Button from '../shared/button';
 import QuantityCounter from '../shared/form/quantityCounter';
 
@@ -15,60 +17,67 @@ export default class ProductForm extends Component {
     const {product, locale} = this.props;
 
     return (
-      <form className="form" onSubmit={console.log('test')}>
-        <fieldset className="form__radio-list product__options-list">
-          <legend className="form__legend form__legend--with-triangle">
-            <FormattedMessage id="lang-option-title" />:
-          </legend>
-          {product[this.props.locale].lang.map((lang, index) => (
-            <div className="form__item-wrapper" key={index}>
-              <input
-                className="form__input sr-only"
-                id={`lang${index}`}
-                type="radio"
-                name="lang"
-                value={lang}
-                defaultChecked={index === 0 ? true : false}
-              />
-              <label
-                className="form__label form__label--radio-btn"
-                htmlFor={`lang${index}`}
-              >
-                {lang}
-              </label>
-            </div>
-          ))}
-        </fieldset>
-        <fieldset className="form__radio-list product__options-list">
-          <legend className="form__legend form__legend--with-triangle">
-            <FormattedMessage id="cover-option-title" />:
-          </legend>
-          {product.cover.map((cover, index) => (
-            <div className="form__item-wrapper" key={index}>
-              <input
-                className="form__input sr-only"
-                id={`${product.color[index]}`}
-                type="radio"
-                name="cover"
-                value={`${cover.childImageSharp.fixed.src} ${
-                  product.color[index]
-                }`}
-                defaultChecked={index === 0 ? true : false}
-              />
-              <label
-                className="form__label form__label--radio-btn"
-                htmlFor={`${product.color[index]}`}
-              >
-                <img src={cover.childImageSharp.fixed.src} alt="" />
-              </label>
-            </div>
-          ))}
-        </fieldset>
-        <QuantityCounter counter={this.state.count} setInput={this.setInput} />
-        <Button theme="primary">
-          <FormattedMessage id="cart-btn-title" />
-        </Button>
-      </form>
+      <StoreContext.Consumer>
+        {() => (
+          <form className="form" onSubmit={console.log('test')}>
+            <fieldset className="form__radio-list product__options-list">
+              <legend className="form__legend form__legend--with-triangle">
+                <FormattedMessage id="lang-option-title" />:
+              </legend>
+              {product[this.props.locale].lang.map((lang, index) => (
+                <div className="form__item-wrapper" key={index}>
+                  <input
+                    className="form__input sr-only"
+                    id={`lang${index}`}
+                    type="radio"
+                    name="lang"
+                    value={lang}
+                    defaultChecked={index === 0 ? true : false}
+                  />
+                  <label
+                    className="form__label form__label--radio-btn"
+                    htmlFor={`lang${index}`}
+                  >
+                    {lang}
+                  </label>
+                </div>
+              ))}
+            </fieldset>
+            <fieldset className="form__radio-list product__options-list">
+              <legend className="form__legend form__legend--with-triangle">
+                <FormattedMessage id="cover-option-title" />:
+              </legend>
+              {product.cover.map((cover, index) => (
+                <div className="form__item-wrapper" key={index}>
+                  <input
+                    className="form__input sr-only"
+                    id={`${product.color[index]}`}
+                    type="radio"
+                    name="cover"
+                    value={`${cover.childImageSharp.fixed.src} ${
+                      product.color[index]
+                    }`}
+                    defaultChecked={index === 0 ? true : false}
+                  />
+                  <label
+                    className="form__label form__label--radio-btn"
+                    htmlFor={`${product.color[index]}`}
+                  >
+                    <img src={cover.childImageSharp.fixed.src} alt="" />
+                  </label>
+                </div>
+              ))}
+            </fieldset>
+            <QuantityCounter
+              counter={this.state.count}
+              setInput={this.setInput}
+            />
+            <Button theme="primary">
+              <FormattedMessage id="cart-btn-title" />
+            </Button>
+          </form>
+        )}
+      </StoreContext.Consumer>
     );
   }
 }
