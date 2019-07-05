bbcc118641b1c4786a135307b867590a3963f9d6
menim <mel198629@gmail.com>
2019-07-05 10:33:31 +0300

    Add product data to CartItem component

--- src/components/Cart/cartItem.js
+++ src/components/Cart/cartItem.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const CartItem = () => <li className="cart__item" />;
+const CartItem = ({productProps, remove}) => {
+  const {lang, cover, title, count} = productProps;
+  const [productCover, color] = cover.split (' ');
+  return (
+    <li className="cart__item">
+      <img src={productCover} alt="" />
+      <span>{title}</span>
+      <span>{lang}</span>
+      <span>{count}</span>
+      <span>₴ {` ${180 * count}`} </span>
+      <button
+        onClick={() => {
+          remove (lang, cover);
+        }}
+        className="cart__remove-btn close close--size-sm"
+      >
+        <span className="sr-only">Удалить</span>
+      </button>
+    </li>
+  );
+};
 
 export default CartItem;
