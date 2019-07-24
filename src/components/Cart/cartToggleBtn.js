5b00bb5c0f0f7c231d1415f7268b8839ff6fdc86
menim <mel198629@gmail.com>
2019-07-24 12:26:33 +0300

    Make cartToggleBtn component back like a function instead class

--- src/components/Cart/cartToggleBtn.js
+++ src/components/Cart/cartToggleBtn.js
@@ -7,78 +7,43 @@ import cartImage from '../../images/cart-icon.svg';
 import {InterfaceContext} from '../../context';
 import CartProductsCount from './cartPrdoductsCount';
 
-class CartToggleBtn extends React.Component {
-  render() {
-    let appInterface = this.context;
-    return (
-      <>
-        <Button onClick={appInterface.cartToggle} className="cart__toggle-btn">
-          <span
-            className={`${
-              this.props.productsInCart === 0
-                ? 'none'
-                : 'cart__products-quantity'
-            } ${
-              appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'
-            }`}
+const CartToggleBtn = ({productsInCart}) => (
+  <InterfaceContext.Consumer>
+    {appInterface => {
+      return (
+        <>
+          <Button
+            onClick={appInterface.cartToggle}
+            className="cart__toggle-btn"
           >
-            <CartProductsCount productsInCart={this.props.productsInCart} />
-          </span>
-          <img
-            className={`cart__basket-img  ${
-              appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'
-            } `}
-            src={cartImage}
-            height="36"
-            width="36"
-            alt=""
-          />
-          <span
-            className={`cart__close close close--theme-secondary ${
-              appInterface.cartStatus ? 'animationZoomIn' : 'animationZoomOut'
-            } `}
-          />
-        </Button>
-      </>
-    );
-  }
-}
-
-CartToggleBtn.contextType = InterfaceContext;
-
-// const CartToggleBtn = ({getProductsInCart}) => (
-//   <InterfaceContext.Consumer>
-//     {(appInterface)=> {
-//       const productsInCart = getProductsInCart();
-//       return (
-//         <>
-//           <Button onClick={appInterface.cartToggle} className="cart__toggle-btn">
-//             <span
-//               className={`${
-//                 productsInCart === 0 ? 'none' : 'cart__products-quantity'
-//               } ${appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'}`}
-//             >
-//               {productsInCart}
-//             </span>
-//             <img
-//               className={`cart__basket-img  ${
-//                 appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'
-//               } `}
-//               src={cartImage}
-//               height="36"
-//               width="36"
-//               alt=""
-//             />
-//             <span
-//               className={`cart__close close close--theme-secondary ${
-//                 appInterface.cartStatus ? 'animationZoomIn' : 'animationZoomOut'
-//               } `}
-//             />
-//           </Button>
-//         </>
-//       );
-//     }}
-//   </InterfaceContext.Consumer>
-// );
+            <span
+              className={`${
+                productsInCart === 0 ? 'none' : 'cart__products-quantity'
+              } ${
+                appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'
+              }`}
+            >
+              <CartProductsCount productsInCart={productsInCart} />
+            </span>
+            <img
+              className={`cart__basket-img  ${
+                appInterface.cartStatus ? 'animationZoomOut' : 'animationZoomIn'
+              } `}
+              src={cartImage}
+              height="36"
+              width="36"
+              alt=""
+            />
+            <span
+              className={`cart__close close close--theme-secondary ${
+                appInterface.cartStatus ? 'animationZoomIn' : 'animationZoomOut'
+              } `}
+            />
+          </Button>
+        </>
+      );
+    }}
+  </InterfaceContext.Consumer>
+);
 
 export default CartToggleBtn;
